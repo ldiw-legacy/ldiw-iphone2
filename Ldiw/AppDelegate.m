fa35cf52716e1cf57409bcd6c1323ba0587328ae
Timo Kallaste <timo.kallaste@mobisolutions.com>
2013-02-18 16:59:19 +0200

    Fixed merge conflicts

--- Ldiw/AppDelegate.m
+++ Ldiw/AppDelegate.m
@@ -8,6 +8,7 @@
 
 #import "AppDelegate.h"
 #import "MainViewController.h"
+#import "LocationManager.h"
 
 @implementation AppDelegate
 
@@ -15,6 +16,9 @@
 {
   self.window = [[UIWindow alloc] initWithFrame:[[UIScreen mainScreen] bounds]];
   // Override point for customization after application launch.
+
+  [self loadServerInformation];
+  
   MainViewController *mainViewController = [[MainViewController alloc] initWithNibName:nil bundle:nil];
   
   UITabBarController *tabBar = [[UITabBarController alloc] init];
@@ -24,6 +28,11 @@
   self.window.backgroundColor = [UIColor whiteColor];
   [self.window makeKeyAndVisible];
   return YES;
+}
+
+- (void)loadServerInformation {
+  MSLog(@"Load server information");
+  
 }
 
 - (void)applicationWillResignActive:(UIApplication *)application
