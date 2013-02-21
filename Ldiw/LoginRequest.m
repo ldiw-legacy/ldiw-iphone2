b4cfd716f68c5a1f8c9a2723973b927b8e5929bc
Sander Paas <sander.paas@mobisolutions.com>
2013-02-21 10:25:50 +0200

    Login

--- Ldiw/LoginRequest.m
+++ Ldiw/LoginRequest.m
@@ -7,7 +7,27 @@
 //
 
 #import "LoginRequest.h"
+#import "LoginViewController.h"
 
 @implementation LoginRequest
+
+#define kLoginPath @"user/login.json"
+#define kLoginServerUrl  @"http://www.letsdoitworld.org/?q=api"
+
++ (void)logInWithParameters:(NSDictionary *)parameters success:(void (^)(NSArray *))success failure:(void (^)(NSError *))failure
+{
+  
+  AFHTTPClient *httpClient = [self sharedHTTPClient];
+  [httpClient postPath:kLoginPath parameters:parameters success:^(AFHTTPRequestOperation *operation, id responseObject) {
+    NSError *jsonError;
+    NSArray *responseArray = [NSJSONSerialization JSONObjectWithData:responseObject options:NSJSONReadingAllowFragments error:&jsonError];
+    success(responseArray);
+  } failure:^(AFHTTPRequestOperation *operation, NSError *error) {
+    MSLog(@"Error %@", error);
+    failure(error);
+  }];
+  
+}
+
 
 @end
