c2b093a4cbb3232a897982dd10d2760f3ddffc32
Johannes Vainikko <johannes.vainikko@mobisolutions.net>
2013-02-21 11:54:58 +0200

    get waste points list commented out

--- Ldiw/ActivityViewController.m
+++ Ldiw/ActivityViewController.m
@@ -147,7 +147,7 @@
     if (result) {
       MSLog(@"Need to load base server information");
       [BaseUrlRequest loadServerInfoForCurrentLocationWithSuccess:^(void) {
-        [self loadWastePointList];
+//        [self loadWastePointList];
         
       } failure:^(void) {
         MSLog(@"Server info loading fail");
