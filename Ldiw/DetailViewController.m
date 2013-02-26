04676517a96f4ad932005358edd8c12a1af34c68
Sander Paas <sander.paas@mobisolutions.com>
2013-02-26 14:12:38 +0200

    Set picture in DetailView

--- Ldiw/DetailViewController.m
+++ Ldiw/DetailViewController.m
@@ -8,6 +8,7 @@
 
 #import "DetailViewController.h"
 #import "LocationManager.h"
+#import "DesignHelper.h"
 #define kViewBackroundColor [UIColor colorWithRed:0.894 green:0.894 blue:0.894 alpha:1] /*#e4e4e4*/
 #define kButtonBackgroundColor [UIColor colorWithRed:0.824 green:0.824 blue:0.824 alpha:1] /*#d2d2d2*/
 
@@ -80,4 +81,35 @@
   picker.delegate = self;
   [self presentViewController:picker animated:YES completion:nil];
 }
+
+- (void)imagePickerController:(UIImagePickerController *)picker didFinishPickingMediaWithInfo:(NSDictionary *)info
+{
+  UIImage *cameraImage = [info objectForKey:UIImagePickerControllerOriginalImage];
+  CFUUIDRef newUniqueID = CFUUIDCreate(kCFAllocatorDefault);
+  CFStringRef newUniqueIDString = CFUUIDCreateString(kCFAllocatorDefault, newUniqueID);
+  //Unique Key
+
+  NSString *key = (__bridge NSString *)newUniqueIDString;
+
+  //ToDo: resize image
+
+  UIImage *resizedImage = [DesignHelper resizeImage:cameraImage];
+  NSData *dataForJpg = UIImageJPEGRepresentation(resizedImage, 0.7);
+
+  //ToDo: set image imageview on detailview:
+
+  //ToDo: save to documents
+
+  //ToDo: save image to database with unique key
+
+  CFRelease(newUniqueID);
+  CFRelease(newUniqueIDString);
+  [self dismissViewControllerAnimated:YES completion:nil];
+  self.imageView.image = cameraImage;
+  self.takePictureButton.alpha = 0;
+  
+  
+}
+
+
 @end
