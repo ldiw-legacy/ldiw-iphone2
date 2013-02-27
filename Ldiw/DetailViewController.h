6bb1cdef43e3f5253795ca49b5fffbca2ef75b68
Sander Paas <sander.paas@mobisolutions.com>
2013-02-27 16:14:59 +0200

    Number and text Keyboard + view and methods

--- Ldiw/DetailViewController.h
+++ Ldiw/DetailViewController.h
@@ -8,7 +8,7 @@
 
 #import <MapKit/MapKit.h>
 
-@interface DetailViewController : UIViewController <UINavigationControllerDelegate, UIImagePickerControllerDelegate, MKMapViewDelegate,UITextFieldDelegate>
+@interface DetailViewController : UIViewController <UINavigationControllerDelegate, UIImagePickerControllerDelegate, MKMapViewDelegate,UITextFieldDelegate,UITextViewDelegate>
 @property (weak, nonatomic) id delegate;
 @property (weak, nonatomic) IBOutlet UIScrollView *scrollView;
 @property (weak, nonatomic) IBOutlet UIImageView *imageView;
--- Ldiw/DetailViewController.m
+++ Ldiw/DetailViewController.m
@@ -11,9 +11,18 @@
 #import "DesignHelper.h"
 #define kViewBackroundColor [UIColor colorWithRed:0.894 green:0.894 blue:0.894 alpha:1] /*#e4e4e4*/
 #define kButtonBackgroundColor [UIColor colorWithRed:0.824 green:0.824 blue:0.824 alpha:1] /*#d2d2d2*/
+@interface DetailViewController ()
+@property (strong, nonatomic) UITextField *textInputField;
+@property (strong, nonatomic) UITextView *myTextInputView;
+@property (strong, nonatomic) UIView *dimView;
+@property (strong, nonatomic) UILabel *insertTextLabel;
+@end
 
-@implementation DetailViewController
-@synthesize scrollView, imageView, mapView;
+
+@implementation DetailViewController 
+
+
+@synthesize scrollView, imageView, mapView, textInputField, dimView,myTextInputView;
 
 - (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
 {
@@ -118,7 +127,17 @@
 }
 - (IBAction)addPressed:(id)sender
 {
-
+  /*
+  UITextField *textfield=[[UITextField alloc] init];
+  textfield.delegate=self;
+  [self.view addSubview:textfield];
+  //[self showCustomKeyboard:textfield];
+  [self showCustomNumPad:textfield];
+  */
+  UITextView *textview=[[UITextView alloc]init];
+  textview.delegate=self;
+  [self.view addSubview:textview];
+  [self showCustomTextPad:textview];
 }
 
 - (IBAction)takePicture:(id)sender {
@@ -158,6 +177,107 @@
   self.takePictureButton.alpha = 0;
   
   
+}
+
+
+- (UIView *)keyboardAccessoryView {
+  //Leftimage
+  CGRect leftRect = CGRectMake(0, 0, 215, 100);
+  CGRect rect = CGRectMake( 0, 0, 320, 100);
+  UIToolbar *tbar = [[UIToolbar alloc] initWithFrame:rect];
+  UIImageView *lefbar = [[UIImageView alloc]initWithFrame:leftRect];
+  lefbar.image = [UIImage imageNamed:@"inputfield_element"];
+  [tbar addSubview:lefbar];
+
+  //Dim
+  CGRect screenRect = [[UIScreen mainScreen] bounds];
+  self.dimView = [[UIView alloc] initWithFrame:screenRect];
+  self.dimView.backgroundColor = [UIColor blackColor];
+  [self.view addSubview:self.dimView];
+  dimView.alpha = 0;
+  [UIView animateWithDuration:0.5
+                   animations:^{self.dimView.alpha = 0.75;}
+                   completion:^(BOOL finished) {
+
+                   }];
+
+  self.navigationController.navigationBarHidden = YES;
+
+  self.insertTextLabel = [[UILabel alloc] initWithFrame:CGRectMake(0, 50, 320, 100)];
+  [self.view addSubview:self.insertTextLabel];
+  self.insertTextLabel.text = @"Enter Comment";
+  self.insertTextLabel.textColor = [UIColor whiteColor];
+  self.insertTextLabel.font = [UIFont fontWithName:@"Caecilia-Heavy" size:28];
+  self.insertTextLabel.textAlignment = UITextAlignmentCenter;
+  self.insertTextLabel.backgroundColor = [UIColor clearColor];
+
+
+  //Button
+  UIImage *confirmImage = [UIImage imageNamed:@"confirm_input_normal.png"];
+  UIButton *confirmButton = [UIButton buttonWithType:UIButtonTypeCustom];
+  confirmButton.frame = CGRectMake(216, 0, confirmImage.size.width, confirmImage.size.height);
+  [confirmButton setBackgroundImage:confirmImage forState:UIControlStateNormal];
+  [confirmButton setBackgroundImage:[UIImage imageNamed:@"confirm_input_pressed.png"] forState:UIControlStateHighlighted];
+  [confirmButton addTarget:self action:@selector(confirmPressed:) forControlEvents:UIControlEventTouchUpInside];
+
+  [tbar addSubview:confirmButton];
+  return tbar;
+  
+}
+
+- (void)showCustomNumPad:(UITextField *)textfield
+{
+  UIView *mytoolbar=[self keyboardAccessoryView];
+  textfield.inputAccessoryView = mytoolbar;
+  [textfield becomeFirstResponder];
+  textfield.keyboardType=UIKeyboardTypeDecimalPad;
+  self.insertTextLabel.text = @"Enter value";
+  self.textInputField = [[UITextField alloc]init];
+  textInputField.font = [UIFont fontWithName:@"Caecilia-Heavy" size:38];
+  textInputField.adjustsFontSizeToFitWidth = YES;
+  textInputField.delegate = self;
+  textInputField.frame = CGRectMake(25, 25, 175, 60);
+  textInputField.keyboardType = UIKeyboardTypeDecimalPad;
+  textInputField.keyboardAppearance = UIKeyboardAppearanceDefault;
+  [mytoolbar addSubview:textInputField];
+  [textInputField becomeFirstResponder];
+}
+
+- (void)showCustomTextPad:(UITextView *)textView
+{
+  UIView *mytoolbar=[self keyboardAccessoryView];
+  textView.inputAccessoryView = mytoolbar;
+  [textView becomeFirstResponder];
+  self.myTextInputView = [[UITextView alloc]init];
+  self.myTextInputView.delegate = self;
+  self.myTextInputView.keyboardType=UIKeyboardTypeDefault;
+  self.myTextInputView.frame = CGRectMake(15, 20, 175, 50);
+  self.myTextInputView.font = [UIFont fontWithName:@"HelveticaNeue" size:14];
+  [mytoolbar addSubview:myTextInputView];
+  [self.myTextInputView becomeFirstResponder];
+  [textView resignFirstResponder];
+}
+
+- (void)textViewDidEndEditing:(UITextView *)textView
+{
+  NSLog(@"textInputView %@",myTextInputView.text);
+}
+
+- (void)textFieldDidEndEditing:(UITextField *)textField
+{
+  NSLog(@"textInputField %@",textInputField.text);
+}
+
+
+-(IBAction)confirmPressed:(id)sender {
+
+  self.navigationController.navigationBarHidden = NO;
+  self.navigationController.navigationBar.alpha = 1;
+  [self.insertTextLabel removeFromSuperview];
+  [self.dimView removeFromSuperview];
+  [textInputField resignFirstResponder];
+  [myTextInputView resignFirstResponder];
+
 }
 
 
