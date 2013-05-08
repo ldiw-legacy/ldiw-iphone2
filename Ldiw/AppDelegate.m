1922872542c353d5a89602c1734f04ab98b2656e
Lauri Eskor <lauri.eskor@mobisolutions.com>
2013-05-08 08:49:56 +0300

    APPS-12. Do not display debug notification.

--- Ldiw/AppDelegate.m
+++ Ldiw/AppDelegate.m
@@ -40,7 +40,6 @@
 {
   [[Database sharedInstance] saveContext];
   [[LocationManager sharedManager] stopLocationManager];
-  [self presentNotificationWithText:@"Application will resign active"];
   // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
   // Use this method to pause ongoing tasks, disable timers, and throttle down OpenGL ES frame rates. Games should use this method to pause the game.
 }
@@ -75,7 +74,7 @@
 
 - (void)presentNotificationWithText:(NSString *)text {
   UILocalNotification *notification = [[UILocalNotification alloc] init];
-  [notification setAlertBody:@"Location changed"];
+  [notification setAlertBody:text];
   [[UIApplication sharedApplication] presentLocalNotificationNow:notification];
 }
 
@@ -98,9 +97,7 @@
   UIApplicationState state = [[UIApplication sharedApplication] applicationState];
   if (state == UIApplicationStateBackground) {
     MSLog(@"Application is in backround");
-    // Fire local notification for testing
-    [self presentNotificationWithText:@"Location changed in background"];
-    
+    // Fire local notification for testing    
     [self setBgTaskIdentifier:[[UIApplication sharedApplication] beginBackgroundTaskWithExpirationHandler:^{
       [[UIApplication sharedApplication] endBackgroundTask:bgTaskIdentifier];
       }]];
